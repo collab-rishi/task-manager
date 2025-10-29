8685a826782b5c2cc95289453c9162463e84959a
collab-rishi <convey.rishi@gmail.com>
2025-10-29 22:43:28 +0530

    Fix: Properly track backend contents

---
+++ backend/src/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const { ServerConfig } = require('./config');
+
+const authRoutes = require("./routes/authRoutes")
+const userRoutes = require("./routes/userRoutes")
+const taskRoutes = require("./routes/taskRoutes")
+const reportRoutes = require("./routes/reportRoutes")
+
+
+const app = express();
+app.use(cors());
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+connectDB();
+
+app.use(express.json());
+
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
+app.use("/api/reports", reportRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+});
